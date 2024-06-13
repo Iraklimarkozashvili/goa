641c21da261c8b4630b23516f4ba505ee2a35d62
iraklimarkozashvili <achimarkozashvili@gmail.com>
2024-06-13 19:43:13 +0200

    commit

---
+++ day07/homework/homework00.py
@@ -0,0 +1,23 @@
+number = "10" 
+
+
+number1 = int (number)
+
+
+print(number1)
+
+number2 = str(number1)
+
+print(number2)
+
+
+
+number3 = float(number1)
+
+
+print(number3)
+
+
+
+
+
---
+++ day07/homework/homework01.py
@@ -0,0 +1,16 @@
+num1 = int(input("enter a number") )
+num2 = int(input("enter a number") )
+num3 = int(input("enter a number") )
+num4 = int(input("enter a number") )
+num5 = int(input("enter a number") )
+
+print(num1 + num2 +num3 + num4 + num5)
+print(num1 - num2 - num3 - num4 - num5)
+print(num1 * num2 * num3 * num4 * num5 )
+print(num1 / num2)
+print(num3 / num4 / num5)
+print(num1 // num2)
+print(num3 // num4 // num5)
+
+print(num1 % num2)
+print(num3 % num4 % num5)
