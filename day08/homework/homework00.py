cb085ef51c322f5d31fb1ab3334763f6220d53cc
iraklimarkozashvili <achimarkozashvili@gmail.com>
2024-06-14 17:46:22 +0200

    commit

---
+++ day08/homework/homework00.py
@@ -0,0 +1,55 @@
+print(30 > 5 )
+print(120 > 60)
+print(240 > 40)
+print(100 > 20)
+print(170 > 50)
+
+
+print(40 < 70)
+print(90 < 120)
+print(10 < 21)
+print(30 < 50)
+print(20 < 50)
+
+
+print(20 ==20)
+print(120 == 120)
+print(140 == 140)
+print(90 == 90)
+print(30 == 30)
+
+
+print(5 >= 5)
+print(50 >= 50)
+print(120 >= 120)
+print(110 >= 110)
+print(30 >= 30)
+
+
+print(30 <= 30)
+print(60 <= 60)
+print(50 <= 50)
+print(110 <= 110)
+print(90 <= 90)
+
+
+print(30 != 220)
+print(90 != 120)
+print(60 != 20)
+print(110 != 60)
+print(20 != 10)
+
+
+
+
+
+
+print(True and False)
+print(True and True)
+print(False and False)
+print(False and True)
+
+print(True or False)
+print(True or True)
+print(False or True)
+print(False or True)
